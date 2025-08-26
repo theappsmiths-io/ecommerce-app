f509e326222ce023db89782457a88529eff2561b
Jearom Roxas <roxasjearom@gmail.com>
2025-08-26 14:40:06 +0800

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/com/theappsmiths/ecommerce/Greeting.kt
@@ -0,0 +1,9 @@
+package com.theappsmiths.ecommerce
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/com/theappsmiths/ecommerce/Platform.kt
@@ -0,0 +1,7 @@
+package com.theappsmiths.ecommerce
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
