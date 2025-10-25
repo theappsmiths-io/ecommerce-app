24bfd96bdfbfcbf3bc4b525b9c2b2b671324ba9e
Jearom Roxas <roxasjearom@gmail.com>
2025-10-25 11:06:13 +0800

    [EC-39] Add flag for showing app bar on TopLevelRoute

--- composeApp/src/commonMain/kotlin/com/theappsmiths/ecommerce/navigation/TopLevelRoute.kt
+++ composeApp/src/commonMain/kotlin/com/theappsmiths/ecommerce/navigation/TopLevelRoute.kt
@@ -12,9 +12,10 @@ enum class TopLevelRoute(
     val route: Route,
     val label: String,
     val icon: ImageVector,
+    val showAppBar: Boolean = false,
 ) {
-    HOME(route = Route.Home, label = "Home", icon = Icons.Default.Home),
-    CATEGORY(route = Route.Category(), label = "Category", icon = Icons.Default.Category),
+    HOME(route = Route.Home, label = "Home", icon = Icons.Default.Home, showAppBar = true),
+    CATEGORY(route = Route.Category(), label = "Category", icon = Icons.Default.Category, showAppBar = true),
     CART(route = Route.Cart, label = "Cart", icon = Icons.Default.ShoppingCart),
     FAVORITE(route = Route.Favorite, label = "Favorite", icon = Icons.Default.Favorite),
     PROFILE(route = Route.Profile, label = "Me", icon = Icons.Default.Person)
